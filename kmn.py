cf5c5540de1cb6ad21742bdf74c7894c42b7f278
TibiI3 <88370126+TibiI3@users.noreply.github.com>
2021-08-03 10:49:19 +0300

    Add files via upload

---
+++ kmn.py
@@ -0,0 +1,44 @@
+import random
+comp = 0
+player = 0
+for i in range(3):
+    print('\t--------Рунд №'+ str(i+1)+'--------')
+    kmn = ['Камень', 'Ножницы', 'Бумага']
+    ran: str = random.choice(kmn)
+    v = input("Что вы выбирете ?")
+    print("Вы выбрали : ", v, '!')
+    kmn = ['Камень', 'Ножницы', 'Бумага']
+    print("Компютор выбрал : ", ran, "!")
+    while True:
+        if (v == ran):
+            print('Оу,ничя!')
+        elif (v == 'Камень') and ran == 'Бумага':
+            comp = comp + 1
+            print('Ты проиграл !')
+        elif (v == "Камень") and ran == 'Ножницы':
+            player = player + 1
+            print('Вы выиграли !')
+        elif (v == 'Ножницы') and ran == 'Камень':
+            comp = comp + 1
+            print('Ты проиграл !')
+        elif (v == 'Ножницы') and ran == 'Бумага':
+            player = player + 1
+            print('Вы выиграли !')
+        elif (v == 'Бумага') and ran == 'Ножницы':
+            comp = comp + 1
+            print('Ты проиграл !')
+        elif (v == 'Бумага') and ran == 'Камень':
+            player = player + 1
+            print('Вы выиграли !')
+        else:
+            print('БАН')
+        break
+print('\t-----Результат-----')
+if i <= 3 and player==comp:
+   print("Ничя")
+elif i <= 3 and player<=comp:
+    print("Ты проиграл!")
+elif i <= 3 and player >= comp:
+    print("Ты выиграл ")
+else:
+    print('Бан')
